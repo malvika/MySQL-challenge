b1cd0d7a04eb8e543d60df82e3bfc6fb6079ab0e
Malvika <malvika.mathur@Gmail.com>
2018-04-27 20:33:20 -0400

    progression of assignment

--- MySQL-challenge.sql
+++ MySQL-challenge.sql
@@ -1,7 +1,5 @@
+-- Using sakila database 
 use sakila; 
-
-SELECT *
-FROM actor;
 
 -- 1a. Display the first and last names of all actors from the table `actor`. 
 SELECT  first_name, last_name
@@ -32,8 +30,7 @@ SELECT country_id
 FROM country 
 WHERE country in ('Afghanistan', 'Bangladesh', 'China') ; 
 
---  3a. Add a `middle_name` column to the table `actor`. Position it between `first_name` and `last_name`. Hint: you will need to specify the data type.
-
+-- 3a. Add a `middle_name` column to the table `actor`. Position it between `first_name` and `last_name`. Hint: you will need to specify the data type.
 ALTER TABLE actor
 ADD COLUMN  middle_name VARCHAR (50) AFTER first_name;
 
@@ -44,3 +41,59 @@ MODIFY COLUMN middle_name BLOB;
 -- 3c. Now delete the `middle_name` column.
 ALTER TABLE actor
 DROP COLUMN middle_name;
+
+-- 4a. List the last names of actors, as well as how many actors have that last name.
+SELECT last_name, COUNT(*) AS `Count`
+FROM actor
+GROUP BY last_name;
+
+-- 4b. List last names of actors and the number of actors who have that last name, but only for names that are shared by at least two actors
+SELECT last_name, COUNT(*) AS `Count`
+FROM actor
+GROUP BY last_name
+HAVING Count > 2; 
+
+-- 4c. Oh, no! The actor `HARPO WILLIAMS` was accidentally entered in the `actor` table as `GROUCHO WILLIAMS`, the name of Harpo's second cousin's husband's yoga teacher. Write a query to fix the record.
+UPDATE actor 
+SET first_name= 'HARPO'
+WHERE first_name='GROUCHO' AND last_name='WILLIAMS';
+
+-- 4d. Perhaps we were too hasty in changing `GROUCHO` to `HARPO`. It turns out that `GROUCHO` was the correct name after all! In a single query, if the first name of the actor is currently `HARPO`, change it to `GROUCHO`. Otherwise, change the first name to `MUCHO GROUCHO`, as that is exactly what the actor will be with the grievous error. BE CAREFUL NOT TO CHANGE THE FIRST NAME OF EVERY ACTOR TO `MUCHO GROUCHO`, HOWEVER! (Hint: update the record using a unique identifier.)
+UPDATE actor 
+SET first_name= 'GROUCHO'
+WHERE first_name='HARPO' AND last_name='WILLIAMS';
+
+-- 5a.  You cannot locate the schema of the `address` table. Which query would you use to re-create it? 
+DESCRIBE sakila.address; 
+
+-- 6a. Use `JOIN` to display the first and last names, as well as the address, of each staff member. Use the tables `staff` and `address`:
+SELECT first_name, last_name, address
+FROM staff s
+INNER JOIN address a
+ON s.address_id = a.address_id;
+
+-- 6b. Use `JOIN` to display the total amount rung up by each staff member in August of 2005. Use tables `staff` and `payment`. 
+SELECT staff.first_name, staff.last_name, SUM(payment.amount) AS 'TOTAL'
+FROM staff  LEFT JOIN payment  ON staff.staff_id = payment.staff_id
+GROUP BY staff.first_name, staff.last_name;
+
+-- 6c. List each film and the number of actors who are listed for that film. Use tables `film_actor` and `film`. Use inner join.
+SELECT film.title, COUNT(film_actor.actor_id) AS 'TOTAL'
+FROM film INNER JOIN film_actor  ON film.film_id = film_actor.film_id
+GROUP BY film.title;
+    
+-- 6d. How many copies of the film `Hunchback Impossible` exist in the inventory system?
+SELECT title, COUNT(inventory_id)
+FROM film f
+INNER JOIN inventory i 
+ON f.film_id = i.film_id
+WHERE title = "Hunchback Impossible";
+-- 6 counts of hunchback
+
+-- 6e. Using the tables `payment` and `customer` and the `JOIN` command, list the total paid by each customer. List the customers alphabetically by last name:
+SELECT last_name, first_name, SUM(amount)
+FROM payment p
+INNER JOIN customer c
+ON p.customer_id = c.customer_id
+GROUP BY p.customer_id
+ORDER BY last_name ASC;
